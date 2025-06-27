00777d9709b10350890f0bba069b0b3b136d89b2
Akash Singh <166646566+singhtheking060605@users.noreply.github.com>
2025-06-27 20:36:04 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -35,7 +35,7 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production'
+    secure:true
   };
 
   res.status(statusCode).cookie("token", token, options).json({
